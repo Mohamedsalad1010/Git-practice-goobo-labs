97d7edec5472ebb8edc8bc375e8e0e9ac134b129
Mohamed salad <abdikariimsalad55@gmail.com>
2026-07-26 11:38:38 +0100

    Add Hello World output

--- file.py
+++ file.py
@@ -1,13 +1,3 @@
 # Simple Python Program
-
-print("Hello Goobo Labs")
-
-print()
-
-print("Hello World")
-
-name = "Caasho"
-
-print("Hello", name)
-
 print("Welcome to Goobo Labs!")
+print("Hello World")
